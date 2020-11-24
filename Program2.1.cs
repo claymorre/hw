a039264f542772a0fb0b41c6e209669417b6a3e3
claymorre <74968900+claymorre@users.noreply.github.com>
2020-11-24 18:07:09 +0500

    Add files via upload

---
+++ Program2.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadanit_2._1
+{
+    struct Types_of
+    {
+        public int number;
+        public string type;
+        public decimal balance;
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Номер счета: {number}\nВид счета: {type}\nБаланс: {balance}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Types_of User;
+            User.number = 81479127;
+            User.type = "Credit";
+            User.balance = 199857.5M;
+            User.DisplayInfo();
+
+
+         
+
+        }
+    }
+}
